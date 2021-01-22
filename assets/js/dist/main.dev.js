04ca533d199c9bdb8d1a04c2a56685e02985e79e
nguyenhung2710 <72492981+nguyenhung2710@users.noreply.github.com>
2021-01-22 17:32:22 +0700

    ver 3

---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,84 @@
+"use strict";
+
+jQuery(document).ready(function ($) {
+  // Smooth scroll
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function () {
+        window.location.hash = hash;
+      });
+    }
+  }); // Modal
+
+  $(document).ready(function () {
+    var modal = $('.modal');
+    var btn = $('.btn');
+    var span = $('.close');
+    modal.show();
+    span.click(function () {
+      modal.hide();
+    });
+    $(window).on('click', function (e) {
+      if ($(e.target).is('.modal')) {
+        modal.hide();
+      }
+    });
+  }); //Change Address
+
+  $(function () {
+    $(".address-item__active").click(function () {
+      $(".address-item").toggle();
+    });
+    $("#addHn").click(function () {
+      $(".address-item__active").val('Hà Nội');
+      $(".address-item").hide();
+    });
+    $("#addDn").click(function () {
+      $(".address-item__active").val('Đà Nẵng');
+      $(".address-item").hide();
+    });
+    $("#addHcm").click(function () {
+      $(".address-item__active").val('TP. HCM');
+      $(".address-item").hide();
+    });
+  }); //Change Size
+
+  $(function () {
+    $(".size-item__active").click(function () {
+      $(".size-item").toggle();
+    });
+    $("#sizeM").click(function () {
+      $(".size-item__active").val('Size M');
+      $(".size-item").hide();
+    });
+    $("#sizeXL").click(function () {
+      $(".size-item__active").val('Size XL');
+      $(".size-item").hide();
+    });
+    $("#sizeXXL").click(function () {
+      $(".size-item__active").val('Size XXL');
+      $(".size-item").hide();
+    });
+  }); //Carousel
+
+  $(function () {
+    $('.carousel-sec6').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true
+    });
+    $('.carousel-sec7').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000
+    });
+  });
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,22 @@ jQuery(document).ready(function ($) {
         }
     });
 
+    // Modal
+    $(document).ready(function () {
+        var modal = $('.modal');
+        var btn = $('.btn');
+        var span = $('.close');
+        modal.show();
+        span.click(function () {
+            modal.hide();
+        });
+        $(window).on('click', function (e) {
+            if ($(e.target).is('.modal')) {
+                modal.hide();
+            }
+        });
+    });
+
     //Change Address
     $(function(){
         $(".address-item__active").click(function(){
@@ -52,12 +68,19 @@ jQuery(document).ready(function ($) {
 
     //Carousel
     $(function() {
-        $('.carousel').slick({
+        $('.carousel-sec6').slick({
             infinite: true,
             slidesToShow: 1,
             slidesToScroll: 1,
             arrows: true,
-            //asNavFor: '.carousel-thumb',
+        });
+        $('.carousel-sec7').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
         });
     })
 });
