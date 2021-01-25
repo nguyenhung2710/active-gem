d639fdec66c830511ebdbfcf57698326971e79b6
nguyenhung2710 <72492981+nguyenhung2710@users.noreply.github.com>
2021-01-25 14:06:05 +0700

    ver 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,19 +13,31 @@ jQuery(document).ready(function ($) {
     });
 
     // Modal
-    $(document).ready(function () {
-        var modal = $('.modal');
-        var btn = $('.btn');
-        var span = $('.close');
-        modal.show();
-        span.click(function () {
-            modal.hide();
+    // $(document).ready(function () {
+    //     var modal = $('.modal');
+    //     var btn = $('.btn');
+    //     var span = $('.close');
+    //     modal.show();
+    //     span.click(function () {
+    //         modal.hide();
+    //     });
+    //     $(window).on('click', function (e) {
+    //         if ($(e.target).is('.modal')) {
+    //             modal.hide();
+    //         }
+    //     });
+    // });
+
+    //Navbar
+    $(function(){
+        $(".navbar-toggler").click(function(){
+            $(".navbar-collapse").toggle(500);
+            //$(".navbar-toggler").hide();
         });
-        $(window).on('click', function (e) {
-            if ($(e.target).is('.modal')) {
-                modal.hide();
-            }
-        });
+        $(".navbar-collapse").before().click(function(){
+            $(".navbar-collapse").hide(500);
+            //$(".navbar-toggler").show();
+        })
     });
 
     //Change Address
@@ -75,12 +87,34 @@ jQuery(document).ready(function ($) {
             arrows: true,
         });
         $('.carousel-sec7').slick({
+            //dots: true,
             infinite: true,
+            speed: 2000,
+            autoplay: true,
+            arrows: false,
             slidesToShow: 3,
             slidesToScroll: 1,
-            arrows: false,
-            autoplay: true,
-            autoplaySpeed: 3000,
+            responsive: [
+                {
+                    breakpoint: 769,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        autoplay: true,
+                        //autoplaySpeed: 2000
+                    }
+                }
+            ]
         });
     })
+
+    
 });
